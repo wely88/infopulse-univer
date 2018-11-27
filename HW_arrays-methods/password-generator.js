b7deaf8490b4c0755b1f622d43624291f6cb935f
wely88 <wely.vfr@gmail.com>
2018-11-27 20:13:06 +0200

    Create password-generator.js

---
+++ HW_arrays-methods/password-generator.js
@@ -0,0 +1,34 @@
+//password with range a-z
+var password = '',
+	passLength = 10,
+	m = 97,
+	n = 122;
+	
+
+for(var i = 0; i < passLength; i++){
+  var passSymbol = Math.floor(Math.random() * (n - m + 1)) + m;
+  password += String.fromCharCode(passSymbol) + ''
+}
+
+console.log(password)
+
+console.log('----------')
+
+//password with ranges a-z, A-Z, 0-9
+
+var password = '',
+	passLength = 10,
+	m = 48,
+	n = 122,
+    i = 0;
+	
+
+while(i < passLength){
+  var passSymbol = Math.floor(Math.random() * (n - m + 1)) + m;
+    if((passSymbol <= 57) || (passSymbol >= 65 && passSymbol <= 90) || (passSymbol >=97)){
+     password += String.fromCharCode(passSymbol) + '';
+     i++;
+    }
+}
+
+console.log(password)
